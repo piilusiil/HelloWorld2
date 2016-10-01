28992255a8fccecf746278a703b47e2ef59e5e7c
Tiina Klooster <tiinaklooster@gmail.com>
2016-10-01 20:21:42 +0200

    kuupäev

--- src/main/tsau.java
+++ src/main/tsau.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class tsau {
     public static void main(String args[]) {
         System.out.println("Tšau! *<3*");
+		System.out.println("Created 01.10.2016");
     }
 }
